6286b3c70238e0e3237b7a46b83ad8b9d72600cb
Loic Poulain <loic.poulain@linaro.org>
2018-06-01 16:57:50 +0200

    lava-slave: Add gpio init for db820c
    Create 96boards LS gpio mapping for db820c.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

---
+++ lava-slave/scripts/db820c-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+db820c_gpios=( 80 29 124 24 62 507 10 8 25 26 23 133)
+ls_gpios=( 23 24 25 26 27 28 29 30 31 32 33 34)
+
+i=0
+for gpio in ${db820c_gpios[@]}; do
+	echo ${gpio} > /sys/class/gpio/export
+	echo out > /sys/class/gpio${gpio}/direction
+	echo 0 > /sys/class/gpio${gpio}/value
+	ln -s /sys/class/gpio/gpio${gpio} /dev/gpio${ls_gpios[${i}]}
+	i=$(($i+1))
+done
