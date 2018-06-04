92f5dffbdb75d845f92466ba42077c78ffc4dbe7
Loic Poulain <loic.poulain@linaro.org>
2018-06-04 18:07:32 +0200

    slave: db410c-init: Fix gpio initialization path
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- lava-slave/scripts/db820c-init.sh
+++ lava-slave/scripts/db820c-init.sh
@@ -6,8 +6,8 @@ ls_gpios=( 23 24 25 26 27 28 29 30 31 32 33 34)
 i=0
 for gpio in ${db820c_gpios[@]}; do
 	echo ${gpio} > /sys/class/gpio/export
-	echo out > /sys/class/gpio${gpio}/direction
-	echo 0 > /sys/class/gpio${gpio}/value
+	echo out > /sys/class/gpio/gpio${gpio}/direction
+	echo 0 > /sys/class/gpio/gpio${gpio}/value
 	ln -s /sys/class/gpio/gpio${gpio} /dev/gpio${ls_gpios[${i}]}
 	i=$(($i+1))
 done
