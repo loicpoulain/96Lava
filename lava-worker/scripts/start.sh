e90605f44cba88f8b013545b390ed386257c51d1
Loic Poulain <loic.poulain@linaro.org>
2018-07-03 10:57:42 +0200

    worker: Fix udev start
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- lava-worker/scripts/start.sh
+++ lava-worker/scripts/start.sh
@@ -4,7 +4,6 @@ service tftpd-hpa start # to retrieve kernel over network (e.g. with u-boot)
 service ser2net start # expose tty port on network
 service rpcbind start # for nfs < 3
 service nfs-kernel-server start # for network rootfs (nfs)
-service udev start # Used to detect and add USB fastboot devices to LXC
 
 MASTER_URL=tcp://localhost:5556
 LOGGER_URL=tcp://localhost:5555
@@ -12,4 +11,5 @@ LOGLEVEL=DEBUG
 . /etc/default/lava-slave
 . /etc/lava-dispatcher/lava-slave
 
+/lib/systemd/systemd-udevd & # Used to detect and add USB fastboot devices to LXC
 /usr/bin/lava-slave --level $LOGLEVEL --master $MASTER_URL --socket-addr $LOGGER_URL --hostname $HOSTNAME
