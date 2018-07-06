00271896df7e371b631f177cb419288755d21b85
Loic Poulain <loic.poulain@linaro.org>
2018-07-06 12:32:12 +0200

    worker: Add FTDI gpio control script
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

---
+++ lava-worker/scripts/dragonboard-ftdi.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Usage dragonboard-ftdi FTDI_SERIAL command
+# commands: reset, reset-fastboot
+
+# Find ftdi sysfs path
+USB_DEVICES=$(find -L /sys/bus/usb/devices/ -maxdepth 3 -name "serial" 2> /dev/null)
+for i in ${USB_DEVICES}; do
+	SERIAL=$(cat $i)
+	if [ ${SERIAL} = $1 ]; then
+		FTDI=$(dirname "$i")
+		break
+	fi
+done
+
+if [ -z ${FTDI} ]; then
+	echo "FTDI with serial '$1' not found!"
+	exit 1
+fi
+
+echo "Found FTDI at ${FTDI}"
+
+# Find base GPIO
+GPIO_BASE=$(cat ${FTDI}/gpio/gpiochip*/base)
+
+if [ -z ${GPIO_BASE} ]; then
+	echo "No GPIO chip associated to the FTDI!"
+	exit 1
+fi
+
+# Dragonboard specific
+GPIO_RESET=${GPIO_BASE}
+GPIO_VOLDOWN=$(expr ${GPIO_RESET} + 1)
+
+echo "GPIO_RESET = ${GPIO_RESET}"
+echo "GPIO_VOLDOWN = ${GPIO_VOLDOWN}"
+
+if [ ! -e /sys/class/gpio/gpio${GPIO_RESET} ]; then
+	echo "Exporting gpio${GPIO_RESET}"
+	echo ${GPIO_RESET} > /sys/class/gpio/export
+fi
+
+if [ ! -e /sys/class/gpio/gpio${GPIO_VOLDOWN} ]; then
+	echo "Exporting gpio${GPIO_VOLDOWN}"
+	echo ${GPIO_VOLDOWN} > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio${GPIO_RESET}/direction
+echo "out" > /sys/class/gpio/gpio${GPIO_VOLDOWN}/direction
+
+GPIO_RESET=/sys/class/gpio/gpio${GPIO_RESET}/value
+GPIO_VOLDOWN=/sys/class/gpio/gpio${GPIO_VOLDOWN}/value
+
+if [ "$2" = "reset-fastboot" ]; then
+	echo 0 > ${GPIO_RESET}
+	echo 0 > ${GPIO_VOLDOWN}
+	sleep 15
+	echo 1 > ${GPIO_RESET}
+	echo 1 > ${GPIO_VOLDOWN}
+elif [ "$2" = "reset" ]; then
+	echo 0 > ${GPIO_RESET}
+	sleep 15
+	echo 1 > ${GPIO_RESET}
+elif [ "$2" = "off" ]; then
+	echo "off-mode not supported"
+elif [ "$2" = "on" ]; then
+	echo 1 > ${GPIO_RESET}
+	echo 1 > ${GPIO_VOLDOWN}
+fi
